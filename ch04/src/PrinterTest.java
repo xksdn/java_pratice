7d9808ad66546dfc7154dc958f8ade9c6394b235
xksdn <xksdn7623@gmail.com>
2023-09-28 17:56:22 +0900

    printer v02

--- ch04/src/PrinterTest.java
+++ ch04/src/PrinterTest.java
@@ -15,11 +15,18 @@ class Printer {
 
     // 용지를 출력하는 메서드
     public void print(int amount) {
-        if (numOfPaper >= amount) {
-            System.out.println(getNumOfPaper() - amount + "장 남았습니다.");
+
+        if (numOfPaper == 0) {
+            System.out.println("용지가 없습니다.");
+        } else if (numOfPaper < amount) {
+            System.out.println(amount + "장 출력합니다." + (amount - numOfPaper) + "장 부족합니다.");
+            numOfPaper -= amount;
         } else {
-            System.out.println("용지가 부족합니다.");
+            // 요청한 양만큼 출력하고 남은 용지 수를 표시
+            System.out.println(amount + "장 출력하였습니다. 남은 용지 수: " + (numOfPaper - amount) + "장");
+            numOfPaper -= amount; // 출력한 용지 수만큼 감소
         }
+
     }
 }
 
@@ -27,14 +34,25 @@ public class PrinterTest {
     public static void main(String[] args) {
         Printer paper = new Printer(); // Printer 클래스의 객체를 생성
 
+
         Scanner scanner = new Scanner(System.in);
 
         // 사용자로부터 용지의 수를 입력받아 설정
         System.out.print("용지를 추가해주세요 : ");
         paper.setNumOfPaper(scanner.nextInt());
 
-        // 사용자로부터 출력할 양을 입력받아 프린터에서 출력
-        System.out.print("몇 장을 출력하시겠어요? : ");
-        paper.print(scanner.nextInt());
+        while (paper.getNumOfPaper() > 0) {
+            // 사용자로부터 출력할 양을 입력받아 프린터에서 출력
+            System.out.print("몇 장을 출력하시겠어요? : ");
+            int printAmount = scanner.nextInt();
+            paper.print(printAmount);
+
+            // 남은 용지 수를 출력
+            if (paper.getNumOfPaper() > 0) {
+                System.out.println("남은 용지 수: " + paper.getNumOfPaper() + "장");
+            } else {
+                System.out.println("남은 용지가 없습니다.");
+            }
+        }
     }
 }
