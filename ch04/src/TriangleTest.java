202bf5cc3440dccfbe5c6e90a1fa6ce3ef31f07f
xksdn <xksdn7623@gmail.com>
2023-09-28 18:53:31 +0900

    triangle v02

--- ch04/src/TriangleTest.java
+++ ch04/src/TriangleTest.java
@@ -28,11 +28,20 @@ class Triangle {
         double area = (side * high) / 2;
         return area;
     }
+
+    public boolean isSameArea(Triangle other) {
+        // 다른 삼각형과 넓이를 비교
+        return this.findArea() == other.findArea();
+    }
 }
 
 public class TriangleTest {
     public static void main(String[] args) {
-        Triangle t = new Triangle(10.0, 5.0);
-        System.out.println("삼각형의 넓이: " + t.findArea());
+        Triangle t1 = new Triangle(10.0, 5.0);
+        Triangle t2 = new Triangle(5.0, 10.0);
+        Triangle t3 = new Triangle(8.0, 8.0);
+
+        System.out.println(t1.isSameArea(t2));
+        System.out.println(t1.isSameArea(t3));
     }
 }
